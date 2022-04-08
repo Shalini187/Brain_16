0c55d89876a2ed93bdd283e4aa82ca0b614b8254
Shalini187 <bhousle83@gmail.com>
2022-04-08 23:29:44 +0530

    view display code

--- source/components/flexLayout/index.tsx
+++ source/components/flexLayout/index.tsx
@@ -1,24 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import React, { createRef, useEffect, useState } from "react";
+import { AppState, Pressable, Text, View } from "react-native";
 import SystemModal from "../modalView";
 
 import { modalStyle, flexStyle } from "../../styles";
-import { randomizeArray, removeDuplicates } from "../../utils";
+import { falsyArray, randomizeArray, removeDuplicates } from "../../utils";
 
 function FlexLayout(props: any) {
     let { state, setState } = props || {};
     let { counter, matches } = state || {};
 
-    let isVisible = false;
+    var isVisible = false;
     let firstCard: any, secondCard: any;
 
     let { centeredView, modalView, textStyle, button, modalText } = modalStyle || {};
     let { container, childrenInVisible, childrenVisible, row } = flexStyle || {};
 
-    const [modalVisible, setModalVisible] = useState<boolean>(false);
+    let [modalVisible, setModalVisible] = useState<boolean>(false);
     let [randomArray, setRandomArray] = useState<Array<string>>([]);
+    let [visible, setVisible] = useState<any>(AppState);
 
     let handleClick = () => setModalVisible(!modalVisible);
+
+    useEffect(() => {
+        randomizeArray(setRandomArray);
+    }, [modalVisible]);
+
+    useEffect(() => {
+        let arr = randomArray?.filter((i) => i != undefined);
+        if (arr?.length) {
+            const subscription = AppState.addEventListener("change", nextAppState => {
+                randomArray?.map((i: any, k: number) => {
+                    let ref: any = createRef({ [k]: false }) || {};
+                    setVisible(ref?.current);
+                })
+            });
+
+            return () => {
+                subscription.remove();
+            };
+        }
+    }, [visible]);
 
     let layoutData = [
         {
@@ -26,63 +47,76 @@ function FlexLayout(props: any) {
             name: 'EXIT',
             onPress: () => {
                 handleClick();
-                setState({ ...state, counter: 0, matches: 0 });
+                resetHandle();
             }
         },
         {
             backgroundColor: 'green',
             name: 'RESTART',
             onPress: () => {
+                resetHandle();
                 randomizeArray(setRandomArray);
-                setState({ ...state, counter: 0, matches: 0 });
             }
         }
     ];
 
-    useEffect(() => {
-        randomizeArray(setRandomArray);
-    }, [modalVisible]);
+    let resetHandle = () => {
+        setVisible(AppState);
+        setState({ ...state, counter: 0, matches: 0 });
+    }
 
     let showCard = (item: any) => {
         if (!isVisible) {
             isVisible = true;
             firstCard = item;
+
+            return;
+        }
+        isVisible = false;
+        secondCard = item;
+
+        checkText();
+
+        if (firstCard?.key == secondCard?.key) return;
+        else return trackCards();
+    }
+
+    let trackCards = () => {
+        if (firstCard?.value == secondCard?.value) {
+            setState({ ...state, matches: matches + 1, counter: counter + 1 });
+            removeDuplicates(randomArray, secondCard?.value, setRandomArray);
         } else {
-            isVisible = false;
-            secondCard = item;
-
-            if (firstCard?.key == secondCard?.key) return;
-            if (firstCard?.value == secondCard?.value) {
-
-                setState({ ...state, matches: matches + 1, counter: counter + 1 });
-                removeDuplicates(randomArray, secondCard?.value, setRandomArray);
-            } else {
-                setState({ ...state, matches: state?.matches, counter: counter + 1 });
-                setTimeout(() => {
-
-                }, 1000);
-            }
+            setState({ ...state, matches: state?.matches, counter: counter + 1 });
+            setTimeout(() => falsyArray(randomArray, setVisible), 1000);
         }
     }
 
-    let renderArray = () => {
-        return (
-            randomArray?.map((item: any, i: number) => {
-                let { value, key } = item || {};
+    let checkText = () => {
+        let obj = Object.assign({}, { [firstCard?.key]: true }, { [secondCard?.key]: true });
+        setVisible({ ...obj });
+    }
 
-                if (item?.value) {
-                    return (
-                        <Pressable key={i} onPress={() => {
-                            showCard(item);
-                        }}>
-                            <View key={key + 20} style={childrenVisible}>
-                                {<Text style={modalText}>{value}</Text>}
-                            </View>
-                        </Pressable>
-                    )
-                } else return <View style={childrenInVisible} />
-            })
-        );
+    let renderArray = () => {
+        let arr = randomArray?.filter((i: any) => i != undefined);
+        if (arr?.length == 0) return <Text style={modalText}>{`You did it at : ${counter} Turns`}</Text>
+        else {
+            return (
+                randomArray?.map((item: any, i: number) => {
+                    let { value, key } = item || {};
+                    if (value) {
+                        return (
+                            <Pressable ref={visible} key={i} onPress={() => {
+                                showCard(item);
+                            }}>
+                                <View key={key + 20} style={childrenVisible}>
+                                    {visible?.[i] ? <Text style={modalText}>{value}</Text> : <></>}
+                                </View>
+                            </Pressable>
+                        )
+                    } else return <View style={childrenInVisible} />
+                })
+            );
+        }
     }
 
     let renderButtons = () => (
